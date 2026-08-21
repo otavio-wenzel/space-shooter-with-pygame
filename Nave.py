2cdb51f787a8cdde46e9ac0ae1c773a2595ecf77
otavio-wenzel <otavio.wenzel@gmail.com>
2026-08-21 17:24:10 -0300

    feat: customização visual da nave

--- Nave.py
+++ Nave.py
@@ -90,63 +90,214 @@ class Nave(ElementoJogo):
         self.atualizar_tiros()
 
     def desenhar(self, tela, vidas=3):
-        # Define a cor conforme o dano da nave
+        # Cores conforme o estado de dano
         if vidas >= 3:
-            cor_atual = self.cor
-        elif vidas == 2:
-            cor_atual = (255, 170, 40)
-        else:
-            cor_atual = (255, 70, 70)
+            cor_corpo = self.cor
+            cor_asas = (20, 110, 180)
 
-        # Formato triangular da nave
-        pontos_nave = [
-            (self.rect.centerx, self.rect.top),
-            (self.rect.left, self.rect.bottom),
-            (self.rect.right, self.rect.bottom)
+        elif vidas == 2:
+            cor_corpo = (255, 170, 40)
+            cor_asas = (180, 90, 20)
+
+        else:
+            cor_corpo = (255, 70, 70)
+            cor_asas = (140, 30, 30)
+
+        centro_x = self.rect.centerx
+        topo = self.rect.top
+        base = self.rect.bottom
+        esquerda = self.rect.left
+        direita = self.rect.right
+
+        # Animação simples dos propulsores
+        oscilacao = (pygame.time.get_ticks() // 100) % 2
+
+        if oscilacao == 0:
+            comprimento_chama = 7
+        else:
+            comprimento_chama = 11
+
+        # Propulsores não aparecem no Game Over
+        if vidas > 0:
+            # Chama esquerda
+            pygame.draw.polygon(
+                tela,
+                (255, 100, 20),
+                [
+                    (centro_x - 9, base - 5),
+                    (centro_x - 4, base - 5),
+                    (
+                        centro_x - 6,
+                        base + comprimento_chama
+                    )
+                ]
+            )
+
+            # Chama direita
+            pygame.draw.polygon(
+                tela,
+                (255, 100, 20),
+                [
+                    (centro_x + 4, base - 5),
+                    (centro_x + 9, base - 5),
+                    (
+                        centro_x + 6,
+                        base + comprimento_chama
+                    )
+                ]
+            )
+
+            # Parte interna amarela das chamas
+            pygame.draw.line(
+                tela,
+                (255, 240, 100),
+                (centro_x - 6, base - 4),
+                (
+                    centro_x - 6,
+                    base + comprimento_chama - 3
+                ),
+                2
+            )
+
+            pygame.draw.line(
+                tela,
+                (255, 240, 100),
+                (centro_x + 6, base - 4),
+                (
+                    centro_x + 6,
+                    base + comprimento_chama - 3
+                ),
+                2
+            )
+
+        # Asa esquerda
+        asa_esquerda = [
+            (centro_x - 5, topo + 14),
+            (esquerda + 2, base - 4),
+            (centro_x - 5, base - 10)
         ]
 
         pygame.draw.polygon(
             tela,
-            cor_atual,
-            pontos_nave
+            cor_asas,
+            asa_esquerda
         )
 
-        # Dano moderado: uma rachadura
+        # Asa direita
+        asa_direita = [
+            (centro_x + 5, topo + 14),
+            (direita - 2, base - 4),
+            (centro_x + 5, base - 10)
+        ]
+
+        pygame.draw.polygon(
+            tela,
+            cor_asas,
+            asa_direita
+        )
+
+        # Corpo principal
+        corpo_nave = [
+            (centro_x, topo),
+            (centro_x - 8, base - 7),
+            (centro_x, base - 12),
+            (centro_x + 8, base - 7)
+        ]
+
+        pygame.draw.polygon(
+            tela,
+            cor_corpo,
+            corpo_nave
+        )
+
+        # Contorno do corpo
+        pygame.draw.polygon(
+            tela,
+            (220, 240, 255),
+            corpo_nave,
+            2
+        )
+
+        # Cabine
+        cabine = pygame.Rect(
+            centro_x - 5,
+            topo + 9,
+            10,
+            13
+        )
+
+        pygame.draw.ellipse(
+            tela,
+            (80, 220, 255),
+            cabine
+        )
+
+        pygame.draw.ellipse(
+            tela,
+            (220, 255, 255),
+            cabine,
+            2
+        )
+
+        # Motores
+        pygame.draw.rect(
+            tela,
+            (80, 80, 95),
+            pygame.Rect(
+                centro_x - 9,
+                base - 9,
+                5,
+                6
+            )
+        )
+
+        pygame.draw.rect(
+            tela,
+            (80, 80, 95),
+            pygame.Rect(
+                centro_x + 4,
+                base - 9,
+                5,
+                6
+            )
+        )
+
+        # Dano moderado
         if vidas == 2:
             pygame.draw.line(
                 tela,
-                (60, 60, 60),
-                (self.rect.centerx, self.rect.centery),
-                (self.rect.right - 5, self.rect.bottom - 5),
+                (55, 45, 40),
+                (centro_x, topo + 18),
+                (centro_x + 7, base - 8),
                 3
             )
 
-        # Dano crítico: duas rachaduras e uma marca central
+        # Dano crítico
         elif vidas <= 1:
             pygame.draw.line(
                 tela,
-                (40, 40, 40),
-                (self.rect.centerx, self.rect.top + 8),
-                (self.rect.left + 8, self.rect.bottom - 5),
+                (40, 30, 30),
+                (centro_x, topo + 6),
+                (centro_x - 7, base - 8),
                 3
             )
 
             pygame.draw.line(
                 tela,
-                (40, 40, 40),
-                (self.rect.centerx, self.rect.centery),
-                (self.rect.right - 6, self.rect.bottom - 4),
+                (40, 30, 30),
+                (centro_x, topo + 18),
+                (centro_x + 8, base - 7),
                 3
             )
 
             pygame.draw.circle(
                 tela,
                 (35, 35, 35),
-                self.rect.center,
+                (centro_x, topo + 22),
                 4
             )
 
-        # Desenha os tiros
+        # Tiros da nave
         for tiro in self.tiros:
             pygame.draw.rect(
                 tela,
